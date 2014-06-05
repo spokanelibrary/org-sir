edb3b6a3a1d38c1aaa49c51753889810eab44d12
Sean Girard <seangirard@yahoo.com>
2014-06-05 09:39:53 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -21,7 +21,7 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand hidden-md hidden-lg" href="<?php echo home_url(); ?>/">
-        <span class="logotype hidden-md"><?php bloginfo('name'); ?></span>
+        <span class="hidden-md">Menu &rarr;</span>
       </a>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
