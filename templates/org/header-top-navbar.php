16f068bb8752f475803cd3423d88e1b68e6c87b8
Sean Girard <seangirard@yahoo.com>
2014-06-05 10:11:02 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,7 +1,7 @@
 
 <header class="navbar-masthead banner navbar navbar-inverse navbar-static-top" role="banner">
   <div class="container">
-    <div class="navbar-header" style="text-align:center;">
+    <div class="navbar-header" style="background:green;">
       <a href="<?php echo home_url(); ?>" style="display:block; border:1px solid red; margin:auto;">
         <!--<span class="text-hide"><?php bloginfo('name'); ?></span>-->
         <img class="img-responsive" src="/assets/img/SIR-masthead.gif" style="margin:auto;">
