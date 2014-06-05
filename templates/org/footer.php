f49752177a4bd9088689e92d7cd6b23fb74daaaa
Sean Girard <seangirard@yahoo.com>
2014-06-05 10:40:26 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -33,4 +33,14 @@
   <span class="visible-lg"></span>
 
 </div>
+
 <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#appId=108585129234285&xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-like" data-href="http://www.facebook.com/spokaneisreading" data-send="true" data-width="450" data-show-faces="true" data-colorscheme="dark"></div>
