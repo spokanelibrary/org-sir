68c093aa612469f58f3d0ce6d22df47cf53b86e0
Sean Girard <seangirard@yahoo.com>
2014-06-05 13:27:48 -0700

    post

--- templates/content-single.php
+++ templates/content-single.php
@@ -8,11 +8,13 @@
       <?php the_content(); ?>
     </div>
 
+    <!--
     <p class="lead text-center text-muted">
       &hellip;
       <i class="glyphicon glyphicon-leaf" style="padding: 0 8px 0 12px;"></i>
       &hellip;
     </p>
+    -->
 
     <?php //get_template_part('templates/entry-meta'); ?>
 
