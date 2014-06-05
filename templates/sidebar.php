7c6f6aafe8dd4a02deea065376f9d57114d01193
Sean Girard <seangirard@yahoo.com>
2014-06-05 10:39:08 -0700

    sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,14 +1,1 @@
-<div class="alert alert-warning" style="border-left-width:4px; margin-top:60px;">
-
-  <p class="lead">
-    <small class="glyphicon glyphicon-bullhorn"></small>
-    In the current issue
-  </p>
-
-  <?php echo do_shortcode('[spl_mailgun_current format=toc link_posts=true]'); ?>
-  <hr>
-  <?php echo do_shortcode('[spl_mailgun_current title="Read the current issue &rarr;" class="btn btn-block btn-warning"]'); ?>
-
-</div>
-
 <?php dynamic_sidebar('sidebar-primary'); ?>
