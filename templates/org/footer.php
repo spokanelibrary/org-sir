be874c8158684345b31e49042bc8deda851e9e32
Sean Girard <seangirard@yahoo.com>
2014-06-05 10:48:02 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -1,26 +1,26 @@
 <hr>
 
+<footer class="content-info container" role="contentinfo">
 
-  <!-- <a name="spl-more">&nbsp;</a> -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#appId=108585129234285&xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 
-  <footer class="content-info container" role="contentinfo">
+  <div class="fb-like" data-href="http://www.facebook.com/spokaneisreading" data-send="true" data-width="450" data-show-faces="true" data-colorscheme="dark"></div>
 
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#appId=108585129234285&xfbml=1";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+  <br>
+  <br>
+  <br>
 
-    <div class="fb-like" data-href="http://www.facebook.com/spokaneisreading" data-send="true" data-width="450" data-show-faces="true" data-colorscheme="dark"></div>
+</footer>
 
-
-  </footer>
-
-  <span class="visible-xs"></span>
-  <span class="visible-sm"></span>
-  <span class="visible-md"></span>
-  <span class="visible-lg"></span>
+<span class="visible-xs"></span>
+<span class="visible-sm"></span>
+<span class="visible-md"></span>
+<span class="visible-lg"></span>
 
