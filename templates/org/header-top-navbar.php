f444fc5a2775b97309718766e543f0d01a57482b
Sean Girard <seangirard@yahoo.com>
2014-06-05 09:32:34 -0700

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,49 +1,10 @@
-<!--<header class="navbar-common banner navbar navbar-fixed-top" role="navigation">
-  <ul class="nav nav-justified">
-    <li>
-      <a href="<?php echo home_url(); ?>/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-home"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-home"></i> Home</span>
-      </a>
-    </li>
-    <li>
-      <a href="/account/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-user"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-user"></i> My Account</span>
-      </a>
-    </li>
-    <li>
-      <a href="/search/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-search"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-search"></i> Search</span>
-      </a>
-    </li>
-    <li>
-      <a href="/browse/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-book"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-book"></i> Browse</span>
-      </a>
-    </li>
-    <li>
-      <a href="/download/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-download-alt"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-download-alt"></i> Download</span>
-      </a>
-    </li>
-    <li>
-      <a href="/connect/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-envelope"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-envelope"></i> Contact Us</span>
-      </a>
-    </li>
-  </ul>
-</header>--><!-- /.navbar -->
 
 <header class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" role="banner">
   <div class="container">
     <div class="navbar-header">
       <a class="navbar-brand" href="http://www.spokanelibrary.org/">
-        <span class="text-hide"><?php bloginfo('name'); ?></span>
+        <!--<span class="text-hide"><?php bloginfo('name'); ?></span>-->
+        <img class="img-responsive" src="/assets/img/SIR-masthead.gif">
       </a>
     </div>
     
