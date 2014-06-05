a33f60fd42ab08aacef5c11f5ebde2aecf76ef18
Sean Girard <seangirard@yahoo.com>
2014-06-05 12:47:54 -0700

    ga.js

--- lib/config.php
+++ lib/config.php
@@ -12,7 +12,7 @@ add_theme_support('jquery-cdn');            // Enable to load jQuery from the Go
 /**
  * Configuration values
  */
-define('GOOGLE_ANALYTICS_ID', 'UA-21176395-1'); // UA-XXXXX-Y
+define('GOOGLE_ANALYTICS_ID', 'UA-51704513-1'); // UA-XXXXX-Y
 define('POST_EXCERPT_LENGTH', 40); // length in words for excerpt_length filter (ref: http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
 
 /**
