d0ed999576d36569cef946fe7a711ba3eaeebaef
Sean Girard <seangirard@yahoo.com>
2014-06-05 10:46:56 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -1,29 +1,9 @@
 <hr>
 
-<div id="spl-more" style="/*background:rgb(100,150,75);*/">
 
   <!-- <a name="spl-more">&nbsp;</a> -->
 
   <footer class="content-info container" role="contentinfo">
-
-    <div class="row">
-      <div class="col-md-12">
-        <span class="pull-right">
-          <img alt="City of Spokane logo" src="http://www.spokanelibrary.org/new/img/city-spokane-logo.png">
-        </span>
-
-        <p>
-          <strong class="text-muted" style="">Spokane Public Library</strong>
-        </p>
-        <address class="text-muted" style="">
-          906 West Main Avenue
-          <br>
-          Spokane, WA 99201
-          <br>
-          509.444.5300
-        </address>
-      </div><!-- /.col -->
-    </div><!-- /.row -->
 
     <div id="fb-root"></div>
     <script>(function(d, s, id) {
@@ -44,4 +24,3 @@
   <span class="visible-md"></span>
   <span class="visible-lg"></span>
 
-</div>
