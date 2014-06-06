8c4f2b6ca3ea1f4dedf16084a8db40c89b6d79d5
Sean Girard <seangirard@yahoo.com>
2014-06-06 09:02:46 -0700

    archive

--- template-archive.php
+++ template-archive.php
@@ -5,6 +5,15 @@ Template Name: Archive (Posts)
 ?>
 
 
+<?php while ( have_posts() ) : the_post(); ?>
+<h1 class="entry-title"><?php the_title(); ?></h1>
+<div class="entry-content">
+<?php the_content(); ?>
+</div><!-- .entry-content -->
+<?php endwhile; // end of the loop. ?>
+
+
+
 <div class="row">
 
 	<div class="col-md-12">
